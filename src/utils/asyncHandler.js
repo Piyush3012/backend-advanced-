9775fd7203379e7f997684b4ae728eed41ef6b0b
piyush deshpande <piyushdeshpande003@gmail.com>
2025-03-24 23:30:11 +0530

    routers and controllers

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //this is asyncHandler code with promises and then catch
 
 const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
     }
 }
